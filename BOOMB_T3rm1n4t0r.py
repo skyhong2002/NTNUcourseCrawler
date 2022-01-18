6215f5b6d2e14caf01cd88c8731119ff86c1e39e
Sky Hong <sky9109@gmail.com>
2022-01-18 17:55:29 +0800

    [update] everything without xml

--- BOOMB_T3rm1n4t0r.py
+++ BOOMB_T3rm1n4t0r.py
@@ -1,18 +1,24 @@
+from cgitb import text
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
 import re
+import xml.etree.cElementTree as ET
+import os
 import random
 
-def load_cookie(fn):
-	with open(fn, 'r') as f:
-		return f.read()
-
-def req(u, cookie):
+def req(url):
+  print('loading', url)
+  fn = 'cookie.txt'
+  with open(fn, 'r') as f:
+    cookie = f.read()
   s = requests.Session()
   s.headers.update({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0', 
                     'Cookie':cookie})
-  res = s.get(u)
+  res = s.get(url)
+  with open("last.html", "wb") as f:
+    f.write(res.content)
+  sleep(5)
   return res
 
 def GetPostContent(result):
@@ -21,9 +27,7 @@ def GetPostContent(result):
     br.replace_with("\n")
   post = soup.find('div', class_='bm')
   article = post.find('div', style = True).text
-
   reaction = [s for s in soup.find_all("a", href=True) if "/ufi/reaction/profile/browser" in s['href']][0].text
-
   return article, reaction
 
 def GetPostComment(result):
@@ -33,87 +37,48 @@ def GetPostComment(result):
   soup = BeautifulSoup(result.text, 'html.parser')
   soup = soup.find('div', id=re.compile('^ufi_*'))
   comments = soup.find('div').find_all('div', id=re.compile('^\d'))
+  all_comment = []
   for comment in comments:
     main_comment = comment.find('div').find('div').text
-    print(main_comment)
-    replied_comment_link = comment.find('div').find_all('a', id=re.compile('^u_0_*'))
+    all_comment.append(main_comment)
+    replied_comment_link = comment.find('div').find_all('a', id=re.compile('^u_0_*'), href=re.compile('^/comment*'))
     for link in replied_comment_link:
-      print(base+link['href'])
-  return 0
+      all_comment = all_comment + GetCommentRepiles(base+link['href'])
+  return all_comment
 
-def beauti4(res):
-
-  tmp = []
-  dates = []
-
-  base = 'https://mbasic.facebook.com'
-  soup = BeautifulSoup(res.text, 'html.parser')
-  for br in soup.find_all("br"):
-    br.replace_with("\n")
-  articles = soup.find_all('article', class_='db dd dn')
-  for a in articles[1:]:
-    art = a.find('div', class_='dt').text
-    date = a.footer.div.text
-    #print(date)
-    tmp.append(art)
-    dates.append(date)
-
-  	#print(art, end='\n\n')
-  #print((articles[6].find('div', class_='dt').text))
-  #title = soup.find('h1', class_='entry-title').text
-
-  return dates, tmp
-
-
-
-def beauti5(res):
-  base = 'https://mbasic.facebook.com'
-  soup = BeautifulSoup(res.text, 'html.parser')
-  print(soup.find('span', text="查看更多貼文"))
-  try:
-    next_page = base + soup.find('div', id='m_group_stories_container').find_all('div')[-1].a['href']  
-  except:
-    next_page = base + soup.find('a', href=True, text="查看更多貼文")['href']  
-  print((next_page))
-  #title = soup.find('h1', class_='entry-title').text
-
-  return next_page
-
-
-
-def beauti6(res):
-
-  soup = BeautifulSoup(res.text, 'html.parser')
-  articles = soup.find_all('article', class_='db dd dn')
-  links = [a.footer.find('a', text="完整動態")['href'].split('/?')[0] for a in articles[1:]]
-  #print(links)
-    #print(art, end='\n\n')
-  #print((articles[6].find('div', class_='dt').text))
-  #title = soup.find('h1', class_='entry-title').text
-
-  return links
+def GetCommentRepiles(url):
+  comment_text = []
+  if 'replies/' not in url:
+    return comment_text
+  result = req(url)
+  soup = BeautifulSoup(result.text, 'html.parser')
+  comments = soup.find('div', id='root').find_all('div', id=re.compile('^\d'))
+  for comment in comments[1:]:
+    comment_text.append(comment.find('div').find('div').text)
+  return comment_text
 
 def load_links():
   with open('articles_links.txt', 'r') as f:
     return [i for i in f.read().split() if i]
 
-next_page = "https://mbasic.facebook.com/groups/143704482352660"
-
-fn = 'cookie.txt'
-cookie = load_cookie(fn)
-fn_links = 'next_pages.txt'
-fn_arts = 'articles.txt'
-fn_arts_links = 'articles_links.txt'
-
-count = 0
-links = load_links()
-for link in links[87:90]:
-  sleep(5)
-  print(link)
-  result = req(link, cookie)
-  with open("post.html", "wb") as f:
-    f.write(result.content)
-  # post_content, post_reaction = GetPostContent(result)
-  # print(post_content, post_reaction)
-  post_comment = GetPostComment(result)
-
+if __name__ == '__main__':
+  group_page = "https://mbasic.facebook.com/groups/143704482352660"
+  fn_links = 'next_pages.txt'
+  fn_arts = 'articles.txt'
+  fn_arts_links = 'articles_links.txt'
+  links = load_links()
+  for link in links:
+    permaID = link.split('/')[-1]
+    if [filename for filename in os.listdir('post_content') if permaID in filename]: 
+      print(permaID, 'exists. skip.')
+      continue
+    try:
+      result = req(link)
+      with open("post.html", "wb") as f:
+        f.write(result.content)
+      post_content, post_reaction = GetPostContent(result)
+      post_comment = GetPostComment(result)
+      # print(link, post_content, post_reaction, post_comment)
+    except:
+      print("failed with loading ", link)
+      sleep(5)
