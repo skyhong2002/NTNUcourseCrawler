9b3a0b742ac0bc3508b9f08c6aad488a2a92eb29
Sky Hong <sky9109@gmail.com>
2022-01-20 02:30:55 +0800

    [update] more and more...

--- BOOMB_T3rm1n4t0r.py
+++ BOOMB_T3rm1n4t0r.py
@@ -76,8 +76,8 @@ if __name__ == '__main__':
   group_page = "https://mbasic.facebook.com/groups/143704482352660"
   fn_arts_links = 'articles_links.txt'
   links = load_links(fn_arts_links)
-  i = 0
-  for link in links:
+  i = 2500
+  for link in links[i+1:]:
     i = i+1
     print(i, end=' ')
     permaID = link.split('/')[-1]
--- post_combine.py
+++ post_combine.py
@@ -25,17 +25,24 @@ def data2xml(permaID, post_content, post_reaction, post_comment):
   return xmlfile
 
 if __name__ == '__main__':
+  illegal_xml_re = re.compile(u'[\x00-\x08\x0b-\x1f\x7f-\x84\x86-\x9f\ud800-\udfff\ufdd0-\ufddf\ufffe-\uffff]')
   with open('allpost.xml', 'w') as f:
     f.write('<?xml version="1.0" encoding="UTF-8"?><b n="articles_modified" book="">  <b n="articles_modified">\n')
+  with open('allpost.txt', 'w') as f:
+    pass
   for filename in os.listdir('post_content'):
-    with open('allpost.xml', 'a') as f:
+    with open('allpost.xml', 'a', encoding='utf-8') as f:
       f.write('<l>')
     contents = load_links(filename)
     for content in contents:
-      content = str(content).encode('utf-8','ignore').decode("utf-8")
-      with open('allpost.xml', 'a') as f:
+      content = illegal_xml_re.sub('', str(content).encode('utf-8','ignore').decode("utf-8"))
+      with open('allpost.xml', 'a', encoding='utf-8') as f:
         f.write(content + ' ')
-    with open('allpost.xml', 'a') as f:
+      with open('allpost.txt', 'a', encoding='utf-8') as f:
+        f.write(content + ' ')
+    with open('allpost.xml', 'a', encoding='utf-8') as f:
       f.write('</l>\n')
-  with open('allpost.xml', 'a') as f:
+    with open('allpost.txt', 'a', encoding='utf-8') as f:
+      f.write('\n')
+  with open('allpost.xml', 'a', encoding='utf-8') as f:
       f.write('</b></b>')
